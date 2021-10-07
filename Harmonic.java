3d8e772c692a3e28567d49f2f0d3945fe4b7091f
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-07 13:39:08 +0530

    Harmonic value of a number

---
+++ Harmonic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+	public class HarmonicNumber {
+
+		public static void main(String[] args) {
+			
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter N to get Nth harmonic value ");
+			
+			int N= sc.nextInt();   
+			double harmonic=0;
+			
+			for(double i=1;i<=N;i++) {
+				double x=1/i;
+				harmonic+=x;
+				
+				}
+			System.out.print("The harmonic value for "+N+" is "+harmonic);
+				
+	}
+
+}
